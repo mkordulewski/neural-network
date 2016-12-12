993275cca85a07fd003b75cc0a2c0773fc049d2b
Michał Kordulewski <michal@kordulewski.pl>
2016-12-12 22:03:49 +0100

    WeightGeneratorTest

---
+++ core/src/test/java/pl/kordulewski/ai/WeightGeneratorTest.java
@@ -0,0 +1,17 @@
+package pl.kordulewski.ai;
+
+import org.junit.Before;
+
+/**
+ * @author Michał Kordulewski
+ */
+public class WeightGeneratorTest {
+
+    private WeightGenerator generator;
+
+    @Before
+    public void before() {
+        generator = new WeightGenerator();
+    }
+
+}
