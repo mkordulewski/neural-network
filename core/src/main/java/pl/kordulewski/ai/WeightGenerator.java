1989dde8e363b31bb0d6572e3002e645daf054fe
Michał Kordulewski <michal@kordulewski.pl>
2016-12-12 22:02:55 +0100

    WeightGenerator

---
+++ core/src/main/java/pl/kordulewski/ai/WeightGenerator.java
@@ -0,0 +1,13 @@
+package pl.kordulewski.ai;
+
+/**
+ * @author Michał Kordulewski
+ */
+public class WeightGenerator {
+
+    public double nextRandomWeight() {
+        // TODO
+        return 0;
+    }
+
+}
