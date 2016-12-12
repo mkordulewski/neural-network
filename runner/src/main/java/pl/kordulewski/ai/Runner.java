0d0e7e549a6b5f859ed7b33c66177e1630ce9374
Michał Kordulewski <michal@kordulewski.pl>
2016-12-12 23:11:11 +0100

    Runner

--- runner/src/main/java/pl/kordulewski/ai/Runner.java
+++ runner/src/main/java/pl/kordulewski/ai/Runner.java
@@ -1,12 +1,31 @@
 package pl.kordulewski.ai;
 
+import pl.kordulewski.ai.generator.PositiveInputDataGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Michał Kordulewski
  */
 public class Runner {
 
     public static void main(String... arg) {
-        // TODO
+        List<String> positiveLearningData = new PositiveInputDataGenerator().generate();
+        // TODO generate negative learning data
+        List<String> negativeLearningData = new ArrayList<>();
+        System.out.println("Positive learning data:");
+        for (String s : positiveLearningData) {
+            System.out.println("  " + s);
+        }
+        System.out.println("Negative learning data:");
+        for (String s : negativeLearningData) {
+            System.out.println("  " + s);
+        }
+        // TODO generate the network
+        // TODO learn the network
+        // TODO generate random testing data
+        // TODO test the network
     }
 
 }
