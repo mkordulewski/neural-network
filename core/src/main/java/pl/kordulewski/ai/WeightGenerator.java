79b960606248813d73e345f056b59137721e1be5
Michał Kordulewski <michal@kordulewski.pl>
2016-12-12 22:08:00 +0100

    WeightGenerator

--- core/src/main/java/pl/kordulewski/ai/WeightGenerator.java
+++ core/src/main/java/pl/kordulewski/ai/WeightGenerator.java
@@ -1,12 +1,18 @@
 package pl.kordulewski.ai;
 
+import java.util.Random;
+
 /**
  * @author Michał Kordulewski
  */
 public class WeightGenerator {
 
+    public static final double MIN = -1;
+    public static final double MAX = 1;
+    private static final Random generator = new Random();
+
     public double nextRandomWeight() {
-        // TODO
+        double randomValue = MIN + (MAX - MIN) * generator.nextDouble();
         return 0;
     }
 
