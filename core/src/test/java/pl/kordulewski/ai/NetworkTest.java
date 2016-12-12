b70cd43d08f1011e8c9d37253488d583160d16ac
Michał Kordulewski <michal@kordulewski.pl>
2016-12-12 22:50:03 +0100

    NetworkTest

--- core/src/test/java/pl/kordulewski/ai/NetworkTest.java
+++ core/src/test/java/pl/kordulewski/ai/NetworkTest.java
@@ -2,6 +2,8 @@ package pl.kordulewski.ai;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /**
  * @author Michał Kordulewski
  */
@@ -10,6 +12,21 @@ public class NetworkTest {
     @Test(expected = RuntimeException.class)
     public void testException() {
         new Network(null, null, null);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testException1() {
+        new Network(null, new ArrayList<Neuron>(), new ArrayList<Neuron>());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testException2() {
+        new Network(new ArrayList<Neuron>(), null, new ArrayList<Neuron>());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testException3() {
+        new Network(new ArrayList<Neuron>(), new ArrayList<Neuron>(), null);
     }
 
 }
