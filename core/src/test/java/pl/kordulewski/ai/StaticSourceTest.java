128ad81b94cf2837dfc3cf8d9b636cb0fa3539f0
Michał Kordulewski <michal@kordulewski.pl>
2016-12-12 21:53:58 +0100

    next tests

--- core/src/test/java/pl/kordulewski/ai/StaticSourceTest.java
+++ core/src/test/java/pl/kordulewski/ai/StaticSourceTest.java
@@ -10,9 +10,21 @@ import static org.assertj.core.api.Assertions.*;
 public class StaticSourceTest {
 
     @Test
-    public void test() {
+    public void test_1() {
+        Source source = new StaticSource(-1);
+        assertThat(source.getValue()).isEqualTo(-1);
+    }
+
+    @Test
+    public void test0() {
         Source source = new StaticSource(0);
         assertThat(source.getValue()).isEqualTo(0);
+    }
+
+    @Test
+    public void test1() {
+        Source source = new StaticSource(1);
+        assertThat(source.getValue()).isEqualTo(1);
     }
 
 }
