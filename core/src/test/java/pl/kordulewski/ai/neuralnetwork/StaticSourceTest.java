478d81535663d82e0032c82b4b108c98490761f2
Michał Kordulewski <michal@kordulewski.pl>
2016-12-13 02:19:41 +0100

    javadocs

--- core/src/test/java/pl/kordulewski/ai/neuralnetwork/StaticSourceTest.java
+++ core/src/test/java/pl/kordulewski/ai/neuralnetwork/StaticSourceTest.java
@@ -11,18 +11,27 @@ public class StaticSourceTest {
 
     private StaticSource source;
 
+    /**
+     * @see StaticSource#StaticSource(double)
+     */
     @Test
     public void test_1() {
         source = new StaticSource(-1);
         assertThat(source.getValue()).isEqualTo(-1);
     }
 
+    /**
+     * @see StaticSource#StaticSource(double)
+     */
     @Test
     public void test0() {
         source = new StaticSource(0);
         assertThat(source.getValue()).isEqualTo(0);
     }
 
+    /**
+     * @see StaticSource#StaticSource(double)
+     */
     @Test
     public void test1() {
         source = new StaticSource(1);
