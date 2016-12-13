46fedcc377acbbe9493e8a43163d421b32cf472a
Michał Kordulewski <michal@kordulewski.pl>
2016-12-13 20:40:45 +0100

    BinaryStepFunctionTest

---
+++ core/src/test/java/pl/kordulewski/ai/neuralnetwork/activation/BinaryStepFunctionTest.java
@@ -0,0 +1,20 @@
+package pl.kordulewski.ai.neuralnetwork.activation;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+/**
+ * @author Michał Kordulewski
+ */
+public class BinaryStepFunctionTest {
+
+    private BinaryStepFunction func = new BinaryStepFunction();
+
+    @Test
+    public void test1() {
+        double result = func.calculate(-1);
+        assertThat(result).isEqualTo(0);
+    }
+
+}
