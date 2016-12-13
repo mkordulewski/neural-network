7317cde676c355a358634e03a4d82cb4cc39e306
Michał Kordulewski <michal@kordulewski.pl>
2016-12-13 22:05:48 +0100

    SigmoidalFunctionTest

--- core/src/test/java/pl/kordulewski/ai/neuralnetwork/activation/SigmoidalFunctionTest.java
+++ core/src/test/java/pl/kordulewski/ai/neuralnetwork/activation/SigmoidalFunctionTest.java
@@ -12,9 +12,17 @@ public class SigmoidalFunctionTest {
     private SigmoidalFunction func = new SigmoidalFunction();
 
     @Test
-    public void test() {
+    public void test1() {
         double result = func.calculate(0);
         assertThat(result).isEqualTo(0.5);
+    }
+
+    @Test
+    public void test2() {
+        for(double i=-10;i<0;i+=0.01) {
+            double result = func.calculate(i);
+            assertThat(result).isLessThan(0.5);
+        }
     }
 
 }
