4de37d9762d170d52fe7fdb617cee40349717195
Michał Kordulewski <michal@kordulewski.pl>
2016-12-13 22:19:37 +0100

    BipolarStepFunctionTest

---
+++ core/src/test/java/pl/kordulewski/ai/neuralnetwork/activation/BiipolarStepFunctionTest.java
@@ -0,0 +1,20 @@
+package pl.kordulewski.ai.neuralnetwork.activation;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+/**
+ * @author Michał Kordulewski
+ */
+public class BiipolarStepFunctionTest {
+
+    private BipolarStepFunction func = new BipolarStepFunction();
+
+    @Test
+    public void test1() {
+        double result = func.calculate(-1);
+        assertThat(result).isEqualTo(-1);
+    }
+
+}
