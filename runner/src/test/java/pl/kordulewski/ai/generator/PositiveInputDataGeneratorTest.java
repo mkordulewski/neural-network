25e1273774e2bf74d1df0053679123c1c8c8eabb
Michał Kordulewski <michal@kordulewski.pl>
2016-12-13 01:01:44 +0100

    PositiveInputDataGeneratorTest

---
+++ runner/src/test/java/pl/kordulewski/ai/generator/PositiveInputDataGeneratorTest.java
@@ -0,0 +1,21 @@
+package pl.kordulewski.ai.generator;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michał Kordulewski
+ */
+public class PositiveInputDataGeneratorTest {
+
+    private PositiveInputDataGenerator generator = new PositiveInputDataGenerator();
+
+    @Test
+    public void test000() {
+        List<String> list = generator.generate();
+        assertThat(list.size()).isGreaterThan(0);
+    }
+}
