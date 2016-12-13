88f64c5f306fbbcc40a57a87ff63390d8a389be8
Michał Kordulewski <michal@kordulewski.pl>
2016-12-13 20:55:53 +0100

    SigmoidalFunction

--- core/src/main/java/pl/kordulewski/ai/neuralnetwork/activation/SigmoidalFunction.java
+++ core/src/main/java/pl/kordulewski/ai/neuralnetwork/activation/SigmoidalFunction.java
@@ -5,9 +5,11 @@ package pl.kordulewski.ai.neuralnetwork.activation;
  */
 public class SigmoidalFunction implements ActivationFunction {
 
+    private static double ALPHA_INDICATOR = 1.0;
+
     @Override
     public double calculate(double value) {
-        return 1.0/(1.0+Math.exp(-1*value));
+        return 1.0/(1.0+Math.exp(-1* ALPHA_INDICATOR *value));
     }
 
 }
