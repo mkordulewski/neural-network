51ef866d2aec8e313284649df17509019bab3746
Michał Kordulewski <michal@kordulewski.pl>
2016-12-25 19:50:19 +0100

    test for NetworkGenerator

--- core/src/test/java/pl/kordulewski/ai/neuralnetwork/generator/NetworkGeneratorTest.java
+++ core/src/test/java/pl/kordulewski/ai/neuralnetwork/generator/NetworkGeneratorTest.java
@@ -2,6 +2,7 @@ package pl.kordulewski.ai.neuralnetwork.generator;
 
 import org.junit.Test;
 import pl.kordulewski.ai.neuralnetwork.Network;
+import pl.kordulewski.ai.neuralnetwork.Neuron;
 
 import static org.assertj.core.api.StrictAssertions.assertThat;
 
@@ -31,6 +32,23 @@ public class NetworkGeneratorTest {
         Network network = generator.generate(1,1,1);
         assertThat(network.getNeuronsInInputLayer().size()).isEqualTo(1);
         assertThat(network.getNeuronsInHiddenLayer().size()).isEqualTo(1);
+        assertThat(network.getNeuronsInOutputLayer().size()).isEqualTo(1);
+    }
+
+    /**
+     * @see NetworkGenerator#generate(int, int, int)
+     */
+    @Test
+    public void test331() {
+        Network network = generator.generate(3,3,1);
+        for (Neuron neuron: network.getNeuronsInOutputLayer()) {
+            neuron.expected(1.0);
+        }
+        for (Neuron neuron: network.getNeuronsInOutputLayer()) {
+            neuron.correctWeights();
+        }
+        assertThat(network.getNeuronsInInputLayer().size()).isEqualTo(3);
+        assertThat(network.getNeuronsInHiddenLayer().size()).isEqualTo(3);
         assertThat(network.getNeuronsInOutputLayer().size()).isEqualTo(1);
     }
 
