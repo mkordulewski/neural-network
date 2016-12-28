cc70601c0c901f3780bc96eb06d5172bdcc3997f
Michał Kordulewski <michal@kordulewski.pl>
2016-12-29 00:45:11 +0100

    javadoc in LearningDataTest

--- core/src/test/java/pl/kordulewski/ai/neuralnetwork/data/LearningDataTest.java
+++ core/src/test/java/pl/kordulewski/ai/neuralnetwork/data/LearningDataTest.java
@@ -1,6 +1,13 @@
 package pl.kordulewski.ai.neuralnetwork.data;
 
+import java.util.List;
+
 /**
+ * @see LearningData
+ * @see LearningData#getInputValues()
+ * @see LearningData#getExpectedOutputData()
+ * @see LearningData#LearningData(List, List)
+ *
  * @author Michał Kordulewski
  */
 public class LearningDataTest {
