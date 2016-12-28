03e1bc3601a2e1a4ebd169820bda84f9bcb3f3e1
Michał Kordulewski <michal@kordulewski.pl>
2016-12-28 01:19:57 +0100

    WeightGenerator.getInstance()

--- core/src/main/java/pl/kordulewski/ai/neuralnetwork/generator/WeightGenerator.java
+++ core/src/main/java/pl/kordulewski/ai/neuralnetwork/generator/WeightGenerator.java
@@ -10,10 +10,18 @@ public class WeightGenerator {
     public static final double MIN = -1;
     public static final double MAX = 1;
     private static final Random generator = new Random();
+    private static WeightGenerator weightGenerator;
 
     public double nextRandomWeight() {
         double randomValue = MIN + (MAX - MIN) * generator.nextDouble();
         return randomValue;
+    }
+
+    public synchronized static WeightGenerator getInstance() {
+        if (weightGenerator == null) {
+            weightGenerator = new WeightGenerator();
+        }
+        return weightGenerator;
     }
 
 }
