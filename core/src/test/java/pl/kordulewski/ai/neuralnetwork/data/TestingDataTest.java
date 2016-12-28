565e6d696ec19895191bd77a97637043610a15ca
Michał Kordulewski <michal@kordulewski.pl>
2016-12-29 00:17:18 +0100

    next test in TestingDataTest

--- core/src/test/java/pl/kordulewski/ai/neuralnetwork/data/TestingDataTest.java
+++ core/src/test/java/pl/kordulewski/ai/neuralnetwork/data/TestingDataTest.java
@@ -14,7 +14,19 @@ public class TestingDataTest {
     private TestingData testingData;
 
     @Test
+    public void test3() {
+        testingData = new TestingData(Arrays.asList());
+        assertThat(testingData.getInputValues().size()).isEqualTo(0);
+    }
+
+    @Test
     public void test1() {
+        testingData = new TestingData(Arrays.asList(1.0));
+        assertThat(testingData.getInputValues().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void test2() {
         testingData = new TestingData(Arrays.asList(1.0, 1.0));
         assertThat(testingData.getInputValues().size()).isEqualTo(2);
     }
