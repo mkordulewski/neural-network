7201f92a1d4e9d2852525afde461af31e23de5f3
Michał Kordulewski <michal@kordulewski.pl>
2016-12-29 00:48:52 +0100

    tests in LearningDataTest

--- core/src/test/java/pl/kordulewski/ai/neuralnetwork/data/LearningDataTest.java
+++ core/src/test/java/pl/kordulewski/ai/neuralnetwork/data/LearningDataTest.java
@@ -1,6 +1,11 @@
 package pl.kordulewski.ai.neuralnetwork.data;
 
+import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.List;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
 
 /**
  * @see LearningData
@@ -11,4 +16,21 @@ import java.util.List;
  * @author Michał Kordulewski
  */
 public class LearningDataTest {
+
+    private LearningData learningData;
+
+    @Test
+    public void test0() {
+        learningData = new LearningData(Arrays.asList(), Arrays.asList());
+        assertThat(learningData.getInputValues().size()).isEqualTo(0);
+        assertThat(learningData.getExpectedOutputData().size()).isEqualTo(0);
+    }
+
+    @Test
+    public void test1() {
+        learningData = new LearningData(Arrays.asList(1.0), Arrays.asList(1.0));
+        assertThat(learningData.getInputValues().size()).isEqualTo(1);
+        assertThat(learningData.getExpectedOutputData().size()).isEqualTo(1);
+    }
+
 }
