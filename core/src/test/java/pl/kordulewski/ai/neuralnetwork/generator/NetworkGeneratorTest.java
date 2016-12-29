0bbde3152b24f7324bf03c428b95cba5297fef1c
Michał Kordulewski <michal@kordulewski.pl>
2016-12-29 02:31:11 +0100

    NetworkGeneratorTest

--- core/src/test/java/pl/kordulewski/ai/neuralnetwork/generator/NetworkGeneratorTest.java
+++ core/src/test/java/pl/kordulewski/ai/neuralnetwork/generator/NetworkGeneratorTest.java
@@ -25,8 +25,6 @@ public class NetworkGeneratorTest {
     @Test
     public void test000() {
         Network network = generator.generate(0,0,0);
-        List<Double> expectedOutputValues = Arrays.asList();
-        network.learnOld(expectedOutputValues);
         assertThat(network.getNeuronsInInputLayer().size()).isEqualTo(0);
         assertThat(network.getNeuronsInHiddenLayer().size()).isEqualTo(0);
         assertThat(network.getNeuronsInOutputLayer().size()).isEqualTo(0);
@@ -38,10 +36,6 @@ public class NetworkGeneratorTest {
     @Test
     public void test111() {
         Network network = generator.generate(1,1,1);
-        List<Double> inputValues = Arrays.asList(-1.0);
-        List<Double> expectedOutputData = Arrays.asList(-1.0);
-        LearningData learningData = new LearningData(inputValues, expectedOutputData);
-        network.learn(Arrays.asList(learningData));
         assertThat(network.getNeuronsInInputLayer().size()).isEqualTo(1);
         assertThat(network.getNeuronsInHiddenLayer().size()).isEqualTo(1);
         assertThat(network.getNeuronsInOutputLayer().size()).isEqualTo(1);
@@ -53,8 +47,6 @@ public class NetworkGeneratorTest {
     @Test
     public void test331() {
         Network network = generator.generate(3,3,1);
-        List<Double> expectedOutputValues = Arrays.asList(1.0);
-        network.learnOld(expectedOutputValues);
         assertThat(network.getNeuronsInInputLayer().size()).isEqualTo(3);
         assertThat(network.getNeuronsInHiddenLayer().size()).isEqualTo(3);
         assertThat(network.getNeuronsInOutputLayer().size()).isEqualTo(1);
@@ -79,43 +71,9 @@ public class NetworkGeneratorTest {
     @Test
     public void test931() {
         Network network = generator.generate(9,3,1);
-        List<Double> expectedOutputValues = Arrays.asList(1.0);
-        network.learnOld(expectedOutputValues);
         assertThat(network.getNeuronsInInputLayer().size()).isEqualTo(9);
         assertThat(network.getNeuronsInHiddenLayer().size()).isEqualTo(3);
         assertThat(network.getNeuronsInOutputLayer().size()).isEqualTo(1);
-    }
-
-    /**
-     * @see NetworkGenerator#generate(int, int, int)
-     */
-    @Test(expected = RuntimeException.class)
-    public void testNotEqualListSizesA() {
-        Network network = generator.generate(9,3,1);
-        List<Double> expectedOutputValues = Arrays.asList();
-        network.learnOld(expectedOutputValues);
-        failBecauseExceptionWasNotThrown(RuntimeException.class);
-    }
-
-    /**
-     * @see NetworkGenerator#generate(int, int, int)
-     */
-    @Test(expected = RuntimeException.class)
-    public void testNotEqualListSizesB() {
-        Network network = generator.generate(9,3,1);
-        List<Double> expectedOutputValues = Arrays.asList(1.0, 1.0);
-        network.learnOld(expectedOutputValues);
-        failBecauseExceptionWasNotThrown(RuntimeException.class);
-    }
-
-    /**
-     * @see NetworkGenerator#generate(int, int, int)
-     */
-    @Test(expected = RuntimeException.class)
-    public void testExpectedValueIsNull() {
-        Network network = generator.generate(9,3,2);
-        List<Double> expectedOutputValues = Arrays.asList(1.0, null);
-        network.learnOld(expectedOutputValues);
     }
 
 }
