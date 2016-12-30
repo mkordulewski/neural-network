8762017010be531cd76f614f481003fd5cc699ee
Michał Kordulewski <michal@kordulewski.pl>
2016-12-30 05:21:30 +0100

    generator of data

--- runner/src/main/java/pl/kordulewski/ai/neuralnetwork/runner/words/generator/Generator.java
+++ runner/src/main/java/pl/kordulewski/ai/neuralnetwork/runner/words/generator/Generator.java
@@ -23,6 +23,21 @@ public class Generator {
         return result;
     }
 
+    public List<String> generateNegativeInputData() {
+        List<String> result = new ArrayList<String>();
+        for (String c1 : new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}) {
+            for (String c2: new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}) {
+                for (String c3: new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}) {
+                    for (String c4: new String[]{"a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}) {
+                        String word = c1 + c2 + c3 + c4;
+                        result.add(word);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     public List<Double> generateListOfDoubles(int size, double value) {
         List<Double> result = new ArrayList<Double>();
         for (int i=0; i<size; i++) {
