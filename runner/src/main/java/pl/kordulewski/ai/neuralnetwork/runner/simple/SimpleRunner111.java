6c01d6e69e81711c6c14fe49cbc5e09adebf5b01
Michał Kordulewski <michal@kordulewski.pl>
2016-12-30 05:09:57 +0100

    simple runners

--- runner/src/main/java/pl/kordulewski/ai/neuralnetwork/runner/simple/SimpleRunner111.java
+++ runner/src/main/java/pl/kordulewski/ai/neuralnetwork/runner/simple/SimpleRunner111.java
@@ -19,15 +19,30 @@ public class SimpleRunner111 {
 
     public void run() {
         Network network = new NetworkGenerator().generate(ActivationFunctionType.UNIPOLAR_SIGMOIDAL, 1, 1, 1);
-        List<Double> inputValues = Arrays.asList(0.666);
+        List<Double> inputValues = Arrays.asList(0.7);
         List<Double> expectedOutputData = Arrays.asList(1.0);
         // testing 1
         network.addInputData(inputValues);
         System.out.print("output before learning: ");
         System.out.format("%10.5f%n", network.getNeuronsInOutputLayer().get(0).getValue());
         // learning 1
-        LearningData learningData = new LearningData(inputValues, expectedOutputData);
-        network.learn(Arrays.asList(learningData));
+        network.learn(Arrays.asList(
+                new LearningData(Arrays.asList(0.0), Arrays.asList(0.0)),
+                new LearningData(Arrays.asList(0.1), Arrays.asList(0.0)),
+                new LearningData(Arrays.asList(0.2), Arrays.asList(0.0)),
+                new LearningData(Arrays.asList(0.33), Arrays.asList(0.0)),
+                new LearningData(Arrays.asList(0.45), Arrays.asList(0.0)),
+                new LearningData(inputValues, expectedOutputData),
+                new LearningData(Arrays.asList(0.5), Arrays.asList(1.0)),
+                new LearningData(Arrays.asList(0.51), Arrays.asList(1.0)),
+                new LearningData(Arrays.asList(0.55), Arrays.asList(1.0)),
+                new LearningData(Arrays.asList(0.6), Arrays.asList(1.0)),
+                new LearningData(Arrays.asList(0.6666), Arrays.asList(1.0)),
+                new LearningData(Arrays.asList(0.77), Arrays.asList(1.0)),
+                new LearningData(Arrays.asList(0.8), Arrays.asList(1.0)),
+                new LearningData(Arrays.asList(0.98), Arrays.asList(1.0)),
+                new LearningData(Arrays.asList(1.0), Arrays.asList(1.0))
+        ));
         // testing 2
         network.addInputData(inputValues);
         System.out.print("output after learning:  ");
