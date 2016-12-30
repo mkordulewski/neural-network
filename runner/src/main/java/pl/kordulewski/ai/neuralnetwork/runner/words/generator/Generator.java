c15da43cc8046f8a6524508feec4f900d175394b
Michał Kordulewski <michal@kordulewski.pl>
2016-12-30 23:44:05 +0100

    generateNegativeInputData method

--- runner/src/main/java/pl/kordulewski/ai/neuralnetwork/runner/words/generator/Generator.java
+++ runner/src/main/java/pl/kordulewski/ai/neuralnetwork/runner/words/generator/Generator.java
@@ -2,11 +2,15 @@ package pl.kordulewski.ai.neuralnetwork.runner.words.generator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author Michał Kordulewski
  */
 public class Generator {
+
+    private static final int NUMBER_OF_WORDS = 100;
+    private static final int WORD_LENGTH = 4;
 
     public List<String> generatePositiveInputData() {
         List<String> result = new ArrayList<String>();
@@ -23,19 +27,27 @@ public class Generator {
         return result;
     }
 
-    public List<String> generateNegativeInputData() {
-        List<String> result = new ArrayList<String>();
-        for (String c1 : new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}) {
-            for (String c2: new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}) {
-                for (String c3: new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}) {
-                    for (String c4: new String[]{"a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}) {
-                        String word = c1 + c2 + c3 + c4;
-                        result.add(word);
-                    }
+    public List<String> generateNegativeInputData(){
+        Random generator = new Random();
+        List<String> negativeInputData = new ArrayList<>();
+        char letter;
+        int chooseLetterSize;
+        for(int i = 0; i < NUMBER_OF_WORDS; i++) {
+            StringBuilder builder = new StringBuilder();
+            for(int j =0; j < WORD_LENGTH; j++ ) {
+                chooseLetterSize = generator.nextInt(2);
+                if (chooseLetterSize == 0) {
+                    int bigLetter = generator.nextInt(26) + 65;
+                    letter = (char) bigLetter;
+                } else {
+                    int smallLetter = generator.nextInt(26) + 97;
+                    letter = (char) smallLetter;
                 }
+                builder.append(letter);
             }
+            negativeInputData.add(builder.toString());
         }
-        return result;
+        return negativeInputData;
     }
 
     public List<Double> generateListOfDoubles(int size, double value) {
