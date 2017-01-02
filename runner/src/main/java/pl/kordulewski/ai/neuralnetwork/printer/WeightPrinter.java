c294f98f980b2eb2b0d8a2a48e82551bb96d2442
Michał Kordulewski <michal@kordulewski.pl>
2017-01-03 00:41:02 +0100

    WeightPrinter

---
+++ runner/src/main/java/pl/kordulewski/ai/neuralnetwork/printer/WeightPrinter.java
@@ -0,0 +1,14 @@
+package pl.kordulewski.ai.neuralnetwork.printer;
+
+import pl.kordulewski.ai.neuralnetwork.Network;
+
+/**
+ * @author Michał Kordulewski
+ */
+public class WeightPrinter {
+
+    public void printWeights(Network network) {
+        // TODO
+    }
+
+}
