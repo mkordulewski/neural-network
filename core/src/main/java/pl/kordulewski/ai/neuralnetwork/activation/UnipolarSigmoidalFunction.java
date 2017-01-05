6ef2b4af78251224eab5c4ed7f8e0935bd0465d6
Michał Kordulewski <michal@kordulewski.pl>
2017-01-06 00:27:13 +0100

    refactoring of UnipolarSigmoidalFunction

--- core/src/main/java/pl/kordulewski/ai/neuralnetwork/activation/UnipolarSigmoidalFunction.java
+++ core/src/main/java/pl/kordulewski/ai/neuralnetwork/activation/UnipolarSigmoidalFunction.java
@@ -5,16 +5,16 @@ package pl.kordulewski.ai.neuralnetwork.activation;
  */
 public class UnipolarSigmoidalFunction implements ActivationFunction {
 
-    private static double ALPHA_INDICATOR = 1.0;
+    private double alpha = 1.0;
 
     @Override
     public double calculate(double value) {
-        return 1.0/(1.0+Math.exp(-1* ALPHA_INDICATOR *value));
+        return 1.0/(1.0+Math.exp(-1* alpha *value));
     }
 
     @Override
     public double derivative(double value) {
-        return (ALPHA_INDICATOR * Math.exp(ALPHA_INDICATOR * value)) / Math.pow(Math.exp(ALPHA_INDICATOR * value) + 1.0, 2);
+        return (alpha * Math.exp(alpha * value)) / Math.pow(Math.exp(alpha * value) + 1.0, 2);
     }
 
 }
